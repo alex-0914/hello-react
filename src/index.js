d85411490da918dd2a7150673600a8be6fa6ee94
alex-0914 <alextangting@gmail.com>
2020-09-16 19:58:56 +0800

    1.18 done

--- src/index.js
+++ src/index.js
@@ -2,138 +2,57 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class LikeButton extends Component {
-  static defaultProps = {
-    likedText: "CANCEL",
-    unlikedText: "Subscribe"
-  }
-  constructor(props) {
-    super(props);
-    this.state = { isLike: false };
+
+class Header extends Component {
+  constructor() {
+    super();
+    console.log("construct");
   }
 
-  handleClickOnLikeButton() {
-    this.setState({
-      isLike: !this.state.isLike
-    })
-    if(this.props.onClick) {
-      this.props.onClick();
+  componentWillMount() {
+    console.log("component will mount");
+  }
+
+  componentDidMount() {
+    console.log("component did mount");
+  }
+
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="title">React Book</h1>
+      </div>
+    )
+  }
+}
+
+class Index extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isShowHeader: true
     }
   }
 
-  render() {
-    return (
-      <button onClick={this.handleClickOnLikeButton.bind(this)}>
-        { this.state.isLike ? this.props.likedText : this.props.unlikedText }
-      </button>
-    )
-  }
-}
-
-class HelloWorld extends Component {
-  constructor() {
-    super();
-  }
-
-  sayHi() {
-    alert("Hello World!");
-  }
-
-  render() {
-    return (
-      <div onClick={this.sayHi.bind(this)}>Hello World!</div>
-    )
-  }
-}
-
-// stateless Component
-// stateful Component
-const HelloWorld2 = (props) => {
-  const sayHi = (event) => alert("Hello World");
-  return (
-    <div onClick={sayHi}>ALEX, GO, GO!</div>
-  );
-}
-// class Index extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       likedText: "No",
-//       unlikedText: "Yes"
-//     }
-//   }
-
-//   handleClickOnChange() {
-//     this.setState({
-//       likedText:'Follow',
-//       unlikedText: "Unfollow"
-//     })
-//   }
-
-
-//   render() {
-//     return (
-//       <div>
-//         <LikeButton 
-//           likedText={this.state.likedText}
-//           unlikedText={this.state.unlikedText}
-//         />
-//         <div>
-//           <button onClick={this.handleClickOnChange.bind(this)}></button>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-const users = [
-  { username: 'Jerry', age: 21, gender: 'male' },
-
-  { username: 'Lily', age: 19, gender: 'female' },
-  { username: 'Lucy', age: 20, gender: 'female' }
-]
-
-
-// class User extends Component {
-//   render() {
-//     const { user } = this.props;
-//     return (
-//       <div>
-//           <div>Name: {user.username} </div>
-//           <div>Age: {user.age} </div>
-//           <div>Gender: {user.gender} </div>
-//           <hr/>
-//       </div>
-//     )
-//   }
-// }
-
-const User = props => {
-    const { user } = props;
-    return (
-      <div>
-        <div>Name: {user.username} </div>
-        <div>Age: {user.age} </div>
-        <div>Gender: {user.gender} </div>
-        <hr/>
-      </div>
-    )
-  }
-class Index extends Component {
-  constructor() {
-    super();
+  handleShowOrHide() {
+    this.setState({
+      isShowHeader: !this.state.isShowHeader
+    })
   }
 
   render() {
     return (
       <div>
-        {users.map((user, i) => <User user={user} key={i}/>)}
+        { this.state.isShowHeader ? <Header/> : null }
+        <button onClick={this.handleShowOrHide.bind(this)}>Hide</button>
       </div>
     )
   }
 }
 
-ReactDOM.render(
-  <Index/>,
-  document.getElementById("root")
-);
+
+ReactDOM.render(<Index/>, document.getElementById("root"));
